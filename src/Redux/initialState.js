d0e44b22579029003cfedfb5119da7c748278c4b
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-30 22:17:50 +0100

    Prepare homepage

--- src/Redux/initialState.js
+++ src/Redux/initialState.js
@@ -1,5 +1,44 @@
 const InitialState = {
-  posts: [],
+  posts: [
+    {
+      id: '1',
+      title: 'Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    },
+    {
+      id: '2',
+      title: 'First article title',
+      shortDescription: 'First short description of the title...',
+      content: 'First main content of the article',
+      publishedDate: '07-07-2022',
+      author: 'Mr. Anderson'
+    },
+    {
+      id: '3',
+      title: 'Second article title',
+      shortDescription: 'Second short description of  the title...',
+      content: 'Second main conent of the article',
+      publishedDate: '12-12-2021',
+      author: 'Marry Poppins'
+    },
+    {
+      id: '4',
+      title: 'Third short description of the title...',
+      shortDescription: 'Third main content of the article',
+      content: 'Third main content of the article',
+      publishedDate: '20-02-2021'
+    },
+    {
+      id: '5',
+      title: 'Fourth shord description of the title...',
+      shortDescription: 'Fourth main content of the article',
+      content: 'Fourth main content of the article',
+      publishedDate: '11-01-2022'
+    },
+  ]
 };
 
 export default InitialState;
